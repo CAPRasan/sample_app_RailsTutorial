41b9141aaa8b0c44f6028ff74e849ecf3f8afcee
yagisan <shoyaaoyagi@gmail.com>
2024-09-07 15:42:38 +0900

    Finish layout and routes

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,8 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_path
-    assert_response :success
-  end
-
   test "should get signup" do
-    get users_signup_path
+    get signup_path
     assert_response :success
   end
 end

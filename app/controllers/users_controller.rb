8b2edd3606bd79106a3ebfc45bae66c8bf0bf746
yagisan <shoyaaoyagi@gmail.com>
2024-09-10 21:01:58 +0900

    Finish signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find_by(id: params[:id])
   end
 
   def new
+    @user = User.new
   end
-end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample app"
+      redirect_to @user
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+    private
+      def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+  end

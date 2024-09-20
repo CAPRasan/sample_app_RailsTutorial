11ebe620fbbd62507697849c9924643808b083ff
yagisan <shoyaaoyagi@gmail.com>
2024-09-20 14:20:09 +0900

    Create follw/unfollow methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name: "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent: :destroy
+    has_many :passive_relationships, class_name: "Relationship",
+                                     foreign_key: "followed_id",
+                                     dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
     attr_accessor :remember_token, :activation_token, :reset_token
     before_create :create_activation_digest
     before_save :downcase_email
@@ -69,6 +77,21 @@ class User < ApplicationRecord
     # 試作feed
     def feed
         Micropost.where("user_id = ?", self.id)
+    end
+
+    # ユーザーをフォローする
+    def follow(other_user)
+        self.following << other_user unless self == other_user
+    end
+
+    # ユーザーのフォローを解除する
+    def unfollow(other_user)
+        self.following.delete(other_user)
+    end
+
+    # 現在のユーザーが他のユーザーをフォローしていればtrueを返す
+    def following?(other_user)
+        self.following.include?(other_user)
     end
 
     private

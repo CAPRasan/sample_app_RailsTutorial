3e21de7ede20c4c4fba8fdf4016414ac2a8a1429
yagisan <shoyaaoyagi@gmail.com>
2024-09-21 16:33:21 +0900

    Fix lint errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,7 +78,7 @@ class User < ApplicationRecord
     def feed
         part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
         Micropost.left_outer_joins(user: :followers)
-                         .where(part_of_feed, {id: id}).distinct
+                         .where(part_of_feed, { id: id }).distinct
                          .includes(:user, image_attachment: :blob)
     end
 
